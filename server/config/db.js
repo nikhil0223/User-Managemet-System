b4dae584507938c7a4122a0b2871a682a74c4c1e
Nikhil <nikhilsharma23062002@gmail.com>
2023-12-29 11:34:32 +0530

    c

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const connectDB = async()=> {
   try {
     mongoose.set('strictQuery', false);
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Database Connected`);
+    console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
   }
